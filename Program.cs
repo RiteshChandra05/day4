e8f7185bfb923d460956ece11d7ad1758c66072c
RiteshChandra05 <kesha.riteshchandragupta@gmail.com>
2016-12-27 23:26:52 -0600

    basic delegate program

--- Program.cs
+++ Program.cs
@@ -4,38 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Interfce
+namespace basicdeligateprgm
 {
-    interface Iprint
-    {
-        void print1();
-    }
-
-    interface Ifax : Iprint
-    {
-        void print2();
-    }
-    public class usage : Ifax
-    {
-        public void print2()
-        {
-            Console.WriteLine("2 copy");
-        }
-        public void print1()
-        {
-            Console.WriteLine("1 copy");
-        }
-
-    }
+    public delegate void delegatepgm(string st);
     class Program
     {
-
-    public static void Main(string[] args)
+       
+       public static void Main(string[] args)
         {
-            usage user = new usage();
-            user.print1();
-            user.print2();
-
+           delegatepgm D1= new delegatepgm(CallDelegate);
+           D1("Hi");
         }
+        public static void CallDelegate(string st)
+       {
+            Console.WriteLine(st);
+       }
     }
 }
