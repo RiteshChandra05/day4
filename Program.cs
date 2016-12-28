68d55795d5d2e627079c33339747995e09412c79
RiteshChandra05 <kesha.riteshchandragupta@gmail.com>
2016-12-27 23:25:50 -0600

    abstract class

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractcls
+{
+    public abstract class Repository 
+    {
+        public abstract void print();
+    }
+
+    class Program : Repository
+    {
+        public override void print()
+        {
+            Console.WriteLine("print abstract");
+        }
+        static void Main(string[] args)
+        {
+            Repository R = new Program();
+            R.print();
+        }
+    }
+}
